9e2d927db94cbe40326c2313f52d50d57fe34349
dogeonK <102578327+dogeonK@users.noreply.github.com>
2022-10-05 16:56:57 +0900

    [UPDATE] 멤버십 삭제 API 구현

--- membership_gradle/src/test/java/com/example/membership/service/MembershipServiceTest.java
+++ membership_gradle/src/test/java/com/example/membership/service/MembershipServiceTest.java
@@ -26,7 +26,7 @@ public class MembershipServiceTest {
     private final String userId = "userId";
     private final MembershipType membershipType = MembershipType.NAVER;
     private final Integer point = 10000;
-    private final Long membershipId = Long.valueOf(1);
+    private final Long membershipId = -1L;
 
     @InjectMocks
     private MembershipService target;
@@ -126,4 +126,29 @@ public class MembershipServiceTest {
         assertThat(result.getMembershipType()).isEqualTo(MembershipType.NAVER);
         assertThat(result.getPoint()).isEqualTo(point);
     }
+    @Test
+    public void 멤버십삭제실패_존재하지않음() {
+        // given
+        doReturn(Optional.empty()).when(membershipRepository).findById(membershipId);
+
+        // when
+        final MembershipException result = assertThrows(MembershipException.class, () -> target.removeMembership(membershipId, userId));
+
+        // then
+        assertThat(result.getErrorResult()).isEqualTo(MembershipErrorResult.MEMBERSHIP_NOT_FOUND);
+    }
+
+    @Test
+    public void 멤버십삭제실패_본인이아님() {
+        // given
+        final Membership membership = membership();
+        doReturn(Optional.of(membership)).when(membershipRepository).findById(membershipId);
+
+        // when
+        final MembershipException result = assertThrows(MembershipException.class, () -> target.removeMembership(membershipId, "notowner"));
+
+        // then
+        assertThat(result.getErrorResult()).isEqualTo(MembershipErrorResult.NOT_MEMBERSHIP_OWNER);
+    }
+
 }
